a9b2bd0714282e843d6f6062098596ce5e39bf47
Masatoshi <msts.oo0131@gmail.com>
2021-08-13 00:06:05 +0900

    blog_path_controller_create

--- Blog/resources/views/blog/index.blade.php
+++ Blog/resources/views/blog/index.blade.php
@@ -1,1 +1,13 @@
-<?php
+@extends('layouts.app')
+
+@section('content')
+    <div class="w-4/5 m-auto text-center">
+        <div class="py-15 border-b border-gray-200">
+            <h1 class="text-6xl">
+                Blog Posts are here !!
+            </h1>
+        </div>
+    </div>
+    
+
+@endsection
