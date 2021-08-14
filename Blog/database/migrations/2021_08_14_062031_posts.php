a3d0da9e59c91517fd1d83cf50cbb52caad7801e
Masatoshi <msts.oo0131@gmail.com>
2021-08-14 16:26:50 +0900

    db_schema_create

--- Blog/database/migrations/2021_08_14_062031_posts.php
+++ Blog/database/migrations/2021_08_14_062031_posts.php
@@ -13,7 +13,16 @@ class Posts extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('posts',function (Blueprint $table){
+          $table->increments('id');
+          $table->string('slug');
+          $table->string('title');
+          $table->longText('content');
+          $table->string('image_path');
+          $table->timestamps();
+          $table->unsignedBigInteger('user_id');
+          $table->foreign('user_id')->references('id')->on('users');
+        });
     }
 
     /**
