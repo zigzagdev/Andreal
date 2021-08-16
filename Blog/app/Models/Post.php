9489c93abd8d3754b3e41b9aeeaea93e36823161
Masatoshi <msts.oo0131@gmail.com>
2021-08-17 00:38:42 +0900

    check_wrongplace_index.blade_blog

--- Blog/app/Models/Post.php
+++ Blog/app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use http\Client\Curl\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
@@ -9,4 +10,21 @@ use Cviebrock\EloquentSluggable\Sluggable;
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
+
+    protected  $fillable = ['title','slug','content','image_path','user_id'];
+
+    public function user()
+      {
+         return $this->belongsTo(User::class);
+      }
+
+     public function sluggable(): array
+       {
+          return [
+              'slug' => [
+                  'source' => 'title'
+              ]
+          ];
+       }
 }
--- Blog/resources/views/blog/index.blade.php
+++ Blog/resources/views/blog/index.blade.php
@@ -9,32 +9,35 @@
         </div>
     </div>
 
- @foreach($posts as $post)
-     <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b boarder-gray-200">
-         <div>
-             <img src="{{ asset('images/' . $post->image_path) }}" alt="">
-         </div>
-         
-         <div>
-             <h2 class="text-gray-600 font-bold text-3xl pb-4">
-                 {{$post->title}}
-             </h2>
+    @if (Auth::check())
+        <div class="pt-15 w-4/5 m-auto">
+            <a
+                href="/blog/create"
+                class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+                Create post
+            </a>
+        </div>
+    @endif
 
-         <span class="text-gray-550">
-           By<span class="font-bold italic text-gray-800">{{$post->user->name}}</span>,Created on {{ date('jS M Y', strtotime($post->updated_at)) }}.
-         </span>
+    @foreach ($posts as $post)
+      <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+            <div>
+                <img src="{{ asset('images/' . $post->image_path) }}" alt="">
+            </div>
+        <div>
+          <h2 class="text-gray-700 font-bold text-5xl pb-4">
+            {{ $post->title }}
+          </h2>
 
-             <p class="text-xl text-gray-700  pt-8 pb-10 leading-8 font-light">
-                {{$post->content}}
-             </p>
+            <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+              {{ $post->content }}
+            </p>
 
-             <a href="" class="uppercase bg-red-600 text-gray-100 font-extrabold py-4 px-8 rounded-3xl">
-                 Keep Reading
-             </a>
-         </div>
-     </div>
- @endforeach
+            <a href="/blog/{{ $post->slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+              Keep Reading
+            </a>
+        </div>
+      </div>
+    @endforeach
 
-
-
-@endsection
+ @endsection
