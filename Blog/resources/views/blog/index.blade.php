a2a6a9a9ea4b87d760f6b3d49b457d042672b21c
Masatoshi <msts.oo0131@gmail.com>
2021-08-19 01:04:02 +0900

    create_post_form

--- Blog/resources/views/blog/index.blade.php
+++ Blog/resources/views/blog/index.blade.php
@@ -1,6 +1,6 @@
- @extends('layouts.app')
+@extends('layouts.app')
 
- @section('content')
+@section('content')
     <div class="w-4/5 m-auto text-center">
         <div class="py-15 border-b border-gray-200">
             <h1 class="text-4xl" style="color: mediumvioletred;">
@@ -20,29 +20,29 @@
     @endif
 
     @foreach ($posts as $post)
-      <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+        <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
             <div>
                 <img src="{{ asset('images/' . $post->image_path) }}" alt="">
             </div>
-        <div>
-          <h2 class="text-gray-700 font-bold text-5xl pb-4">
-            {{ $post->title }}
-          </h2>
+            <div>
+                <h2 class="text-gray-700 font-bold text-5xl pb-4">
+                    {{ $post->title }}
+                </h2>
 
-          <span class="text-gray-500">
-            By <span class="font-bold italic text-gray-800">{{ $post->Matthew }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
+                <span class="text-gray-500">
+            By <span class="font-bold italic text-gray-800">{{ $post->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
           </span>
-            {{-- $post->user->name --}}
+                {{-- $post->user->name --}}
 
-            <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-              {{ $post->content }}
-            </p>
+                <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+                    {{ $post->content }}
+                </p>
 
-            <a href="/blog/{{ $post->slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
-              Keep Reading
-            </a>
+                <a href="/blog/{{ $post->slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+                    Keep Reading
+                </a>
+            </div>
         </div>
-      </div>
     @endforeach
 
- @endsection
+@endsection
