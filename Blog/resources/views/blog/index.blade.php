534088dce1cd64d389dff7948685313c879ef4ad
Masatoshi <msts.oo0131@gmail.com>
2021-08-22 19:42:50 +0900

    index_authorname_determine

--- Blog/resources/views/blog/index.blade.php
+++ Blog/resources/views/blog/index.blade.php
@@ -39,9 +39,8 @@
                 </h2>
 
                 <span class="text-gray-500">
-            By <span class="font-bold italic text-gray-800">{{ $post->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
-          </span>
-                {{-- $post->user->name --}}
+                   By <span class="font-bold italic text-gray-800">Matthew</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
+                </span>
 
                 <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
                     {{ $post->content }}
